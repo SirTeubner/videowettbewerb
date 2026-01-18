b799d9ae8af65f2b3170692152d9cbf98e29f2b1
Noah Teubner <NTeubner@schueler.hakzell.at>
2026-01-18 19:51:15 +0100

    navbar-inhalte für unregistrierte nutzer verfügbar gemacht

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite('resources/css/app.css')
 </head>
-
 <body>
+<!-- Abfrage ob der Nutzer angemeldet ist oder ob nicht
 <h1 class="text-3xl font-bold">
     @if (Route::has('login'))
         @auth
@@ -19,8 +19,17 @@
                 <li class="font-bold underline"><a href="{{ url('/login') }}">Anmeldung</a></li>
             </div>
         @endauth
-
     @endif
 </h1>
+Ende Abfrage -->
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+</x-app-layout>
+
+
 </body>
 </html>
