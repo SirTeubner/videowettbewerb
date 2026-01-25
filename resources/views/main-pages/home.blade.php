e7a955c6500e08df4c29efeed36a16c3e51f847d
Noah Teubner <NTeubner@schueler.hakzell.at>
2026-01-25 17:50:20 +0100

    anpassung darstellung kleinere displays

--- resources/views/main-pages/home.blade.php
+++ resources/views/main-pages/home.blade.php
@@ -1,23 +1,19 @@
 <x-app-layout>
 
-    <!-- Hauptbereich -->
-    <div class="bg-black text-white px-20 py-12 max-w-7xl mx-auto my-10 rounded-full space-y-10">
+    <div class="bg-black text-white px-6 sm:px-10 md:px-20 py-8 sm:py-16 md:py-8 max-w-7xl mx-auto my-10 md:rounded-full sm:rounded-none space-y-10">
 
-        <!-- Überschrift -->
-        <h1 class="text-5xl font-bold text-center leading-tight">
+        <h1 class="text-5-xl sm:text-4xl md:text-4xl font-bold text-center leading-tight">
             Der erste Pinzgauer Videowettbewerb
         </h1>
 
-        <!-- Text und Bild -->
-        <div class="flex flex-col md:flex-row items-center justify-between gap-12">
+        <div class="flex flex-col md:flex-row items-center justify-between gap-8 sm:gap-10 md:gap-12 px-4 sm:px-8 md:px-10">
 
-            <!-- Textblock -->
             <div class="flex-1 space-y-6">
-                <h2 class="text-2xl font-semibold">
+                <h2 class="text-lg sm:text-xl md:text-2xl font-semibold">
                     Was macht den Pinzgau für dich besonders?
                 </h2>
 
-                <p class="text-base leading-relaxed">
+                <p class="text-sm sm:text-base leading-relaxed text-justify">
                     Unter diesem Motto starten wir (das sind Arian, David, Noah und Samuel) den ersten Pinzgauer Video- und Fotowettbewerb für Jugendliche. Besser gesagt für Schüler und Schülerinnen der dritten und vierten Klasse Unterstufe. Der Videowettbewerb wird hierbei im Gruppenformat ausgetragen, während es beim Fotowettbewerb um Einzelleistungen geht. Unser Ziel ist es, dem Pinzgau ein neues Gesicht zu geben. Dabei geht es auch um euch, denn egal, was den Pinzgau für euch besonders macht, wir wollen es sehen. Ob Sport, Brauchtum, die Menschen oder etwas ganz anderes, ihr entscheidet. Also, startet gleich durch, wir freuen uns auf eure Videos und Fotos.
                 </p>
 
@@ -27,18 +23,14 @@
                 </a>
             </div>
 
-            <!-- Bildblock -->
             <div class="flex-1">
                 <img src="{{ asset('images/kuh.png') }}"
                      alt="kuh"
-                     class="w-full max-w-xl mx-auto rounded-2xl object-cover shadow-xl">
+                     class="w-full max-w-sm sm:max-w-md md:max-w-xl mx-auto rounded-2xl object-cover shadow-xl">
             </div>
 
         </div>
 
-        <!-- Lila Balken -->
-        <div class="h-2 bg-[#A594F9] rounded-full w-1/2 mx-auto"></div>
-
+        <div class="hidden md:flex h-1.5 bg-brandpurple rounded-full w-1/3 mx-auto mt-8"></div>
     </div>
-
 </x-app-layout>
