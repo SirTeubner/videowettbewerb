04f86decd22c57a2223df4da20c2a282f7685a0b
Noah Teubner <NTeubner@schueler.hakzell.at>
2026-01-26 08:56:49 +0100

    ueberuns-page befüllt

--- resources/views/main-pages/ueberuns.blade.php
+++ resources/views/main-pages/ueberuns.blade.php
@@ -1,5 +1,147 @@
 <x-app-layout>
-<h1>
-    Viele wichtige Infos über uns
-</h1>
+
+    <!-- Abschnitt 1 -->
+    <div class="bg-black text-white px-10 md:px-20 py-12 md:py-8 max-w-7xl mx-auto my-10 lg:rounded-full md:rounded-none space-y-10">
+
+        <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold text-center leading-tight">
+            Arian
+        </h1>
+
+        <div class="flex flex-col md:flex-row items-center justify-between gap-8 sm:gap-10 md:gap-12 px-4 sm:px-8 md:px-10">
+            <div class="flex-1 space-y-6">
+                <h2 class="text-lg sm:text-xl md:text-2xl font-semibold">
+                    Zuständigkeitsbereich: Marketing
+                </h2>
+
+                <p class="text-sm sm:text-base leading-relaxed text-justify pb-5">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                    erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+                    ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+                    ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+                    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+                    dolore magna aliquyam erat, sed Lorem ipsum dolor sit amet,
+                    consetetur sadipscing elitr, sed diam n clita kasd gubergren
+                </p>
+            </div>
+
+            <div class="flex-1">
+                <img src="{{ asset('images/kuh.png') }}"
+                     alt="kuh"
+                     class="w-full max-w-sm sm:max-w-md md:max-w-xl mx-auto rounded-2xl object-cover shadow-xl">
+            </div>
+        </div>
+
+        <div class="hidden lg:flex h-1.5 bg-brandpurple rounded-full w-1/3 mx-auto mt-8"></div>
+    </div>
+    <!-- Ende Abschnitt 1 -->
+
+    <!-- Abschnitt 2 -->
+    <div class="bg-black text-white px-10 md:px-20 py-12 md:py-8 max-w-7xl mx-auto my-10 lg:rounded-full md:rounded-none space-y-10">
+
+        <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold text-center leading-tight">
+            Dave
+        </h1>
+
+        <div class="flex flex-col md:flex-row items-center justify-between gap-8 sm:gap-10 md:gap-12 px-4 sm:px-8 md:px-10">
+            <div class="flex-1 space-y-6">
+                <h2 class="text-lg sm:text-xl md:text-2xl font-semibold">
+                    Zuständigkeitsbereich: Partnerbetreuung
+                </h2>
+
+                <p class="text-sm sm:text-base leading-relaxed text-justify pb-5">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                    erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+                    ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+                    ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+                    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+                    dolore magna aliquyam erat, sed Lorem ipsum dolor sit amet,
+                    consetetur sadipscing elitr, sed diam n clita kasd gubergren
+                </p>
+            </div>
+
+            <div class="flex-1">
+                <img src="{{ asset('images/kuh.png') }}"
+                     alt="kuh"
+                     class="w-full max-w-sm sm:max-w-md md:max-w-xl mx-auto rounded-2xl object-cover shadow-xl">
+            </div>
+        </div>
+
+        <div class="hidden lg:flex h-1.5 bg-brandpurple rounded-full w-1/3 mx-auto mt-8"></div>
+    </div>
+    <!-- Ende Abschnitt 2 -->
+
+    <!-- Abschnitt 3 -->
+    <div class="bg-black text-white px-10 md:px-20 py-12 md:py-8 max-w-7xl mx-auto my-10 lg:rounded-full md:rounded-none space-y-10">
+
+        <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold text-center leading-tight">
+            Noah
+        </h1>
+
+        <div class="flex flex-col md:flex-row items-center justify-between gap-8 sm:gap-10 md:gap-12 px-4 sm:px-8 md:px-10">
+            <div class="flex-1 space-y-6">
+                <h2 class="text-lg sm:text-xl md:text-2xl font-semibold">
+                    Zuständigkeitsbereich: Website
+                </h2>
+
+                <p class="text-sm sm:text-base leading-relaxed text-justify pb-5">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                    erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+                    ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+                    ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+                    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+                    dolore magna aliquyam erat, sed Lorem ipsum dolor sit amet,
+                    consetetur sadipscing elitr, sed diam n clita kasd gubergren
+                </p>
+            </div>
+
+            <div class="flex-1">
+                <img src="{{ asset('images/kuh.png') }}"
+                     alt="kuh"
+                     class="w-full max-w-sm sm:max-w-md md:max-w-xl mx-auto rounded-2xl object-cover shadow-xl">
+            </div>
+        </div>
+
+        <div class="hidden lg:flex h-1.5 bg-brandpurple rounded-full w-1/3 mx-auto mt-8"></div>
+    </div>
+    <!-- Ende Abschnitt 3 -->
+
+    <!-- Abschnitt 4 -->
+    <div class="bg-black text-white px-10 md:px-20 py-12 md:py-8 max-w-7xl mx-auto my-10 lg:rounded-full md:rounded-none space-y-10">
+
+        <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold text-center leading-tight">
+            Samuel
+        </h1>
+
+        <div class="flex flex-col md:flex-row items-center justify-between gap-8 sm:gap-10 md:gap-12 px-4 sm:px-8 md:px-10">
+            <div class="flex-1 space-y-6">
+                <h2 class="text-lg sm:text-xl md:text-2xl font-semibold">
+                    Zuständigkeitsbereich: Corporate Design
+                </h2>
+
+                <p class="text-sm sm:text-base leading-relaxed text-justify pb-5">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                    erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+                    ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+                    ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+                    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+                    dolore magna aliquyam erat, sed Lorem ipsum dolor sit amet,
+                    consetetur sadipscing elitr, sed diam n clita kasd gubergren
+                </p>
+            </div>
+
+            <div class="flex-1">
+                <img src="{{ asset('images/kuh.png') }}"
+                     alt="kuh"
+                     class="w-full max-w-sm sm:max-w-md md:max-w-xl mx-auto rounded-2xl object-cover shadow-xl">
+            </div>
+        </div>
+
+        <div class="hidden lg:flex h-1.5 bg-brandpurple rounded-full w-1/3 mx-auto mt-8"></div>
+    </div>
+    <!-- Ende Abschnitt 4 -->
+
 </x-app-layout>
