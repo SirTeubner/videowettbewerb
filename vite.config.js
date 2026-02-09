e583ac77f572b53be48c74f6ccab701394dbbcde
Noah Teubner <NTeubner@schueler.hakzell.at>
2026-02-09 19:55:02 +0100

    update vite for production build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/build/',
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        emptyOutDir: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
