508a2b85b490484aa08dca2a276e243ecc30b9fd
Noah Teubner <NTeubner@schueler.hakzell.at>
2026-02-19 11:04:13 +0100

    remove dark mode features

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-brandpurple dark:border-indigo-600 text-start text-base font-medium text-brandpurple dark:text-indigo-300 bg-black dark:bg-indigo-900/50'
-            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-white dark:text-gray-400 focus:text-brandpurple dark:focus:text-gray-200 focus:bg-black dark:focus:bg-gray-700 focus:border-brandpurple dark:focus:border-gray-600';
+            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-brandpurple text-start text-base font-medium text-brandpurple bg-black'
+            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-white focus:text-brandpurple focus:bg-black focus:border-brandpurple';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
